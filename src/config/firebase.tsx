a4a9df3f35cde8d24679ff930e0588ec54a80cef
Ndubuisi Agbo <agbondu9@gmail.com>
2024-08-12 21:46:58 +0100

    update-secret

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -43,27 +43,13 @@ import {
   MEASUREMENT_ID,
 } from '@env';
 
-// const apiKey = API_KEY;
-// const authDomain = AUTH_DOMAIN;
-// const projectId = PROJECT_ID;
-// const storageBucket = STORAGE_BUCKET;
-// const messagingSenderId = MESSAGING_SENDER;
-// const appId = APP_ID;
-// const measurementId = MEASUREMENT_ID;
-
-const apiKey = 'AIzaSyAc5n_xuxFm2GHXyxfuyHCOOOdyaOg_T-4';
-
-const authDomain = 'urban-article.firebaseapp.com';
-
-const projectId = 'urban-article';
-
-const storageBucket = 'urban-article.appspot.com';
-
-const messagingSenderId = '196832293772';
-
-const appId = '1:196832293772:web:9eb1fc1f06dc211789dca9';
-
-const measurementId = 'G-9CZQ525E5V';
+const apiKey = API_KEY;
+const authDomain = AUTH_DOMAIN;
+const projectId = PROJECT_ID;
+const storageBucket = STORAGE_BUCKET;
+const messagingSenderId = MESSAGING_SENDER;
+const appId = APP_ID;
+const measurementId = MEASUREMENT_ID;
 
 const app = initializeApp({
   apiKey,
