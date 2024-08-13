9d33307bf0de19971f4a94bffe2f26526505e6c1
Ndubuisi Agbo <agbondu9@gmail.com>
2024-08-13 01:02:26 +0100

    import-drawer

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,32 @@
  *
  * @format
  */
-
+import 'react-native-gesture-handler';
+import 'react-native-safe-area-context';
+import 'react-native-reanimated';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {StatusBar, Text, useColorScheme, View} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {screenNames} from './screen';
+import {CustomDrawerContent} from './components';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {wp} from './config/appConfig';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Drawer = createDrawerNavigator();
 
+function HomeStack() {
+  return (
+    <View
+      style={{
+        marginTop: 200,
+        flex: 1,
+      }}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -30,16 +38,32 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaProvider style={backgroundStyle}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
 
-      <View>
-        <Text>Welcome</Text>
-      </View>
-    </SafeAreaView>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContent={props => <CustomDrawerContent {...props} />}
+          screenOptions={{
+            drawerStyle: {
+              backgroundColor: 'white',
+              width: wp('70%'),
+            },
+          }}
+          initialRouteName={screenNames.homeStack}>
+          <Drawer.Screen
+            options={{
+              headerShown: false,
+            }}
+            name={screenNames.homeStack}
+            component={HomeStack}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
